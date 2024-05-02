506112a3bc92549a788e727d9e926a7d49b662a3
LauMez <lautaromartinez302@gmail.com>
2024-05-02 18:52:59 -0300

    De nuevo

--- src/routes/route1.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-
-export const exepleRouter = Router()
-
-//exepleRouter.app('/', (req, res) => {})
-
-//exepleRouter.post('/', (req, res) => {})
-
-//expleRouter.get('/:id', (req, res) =>{})
